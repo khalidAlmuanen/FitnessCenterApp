e4ea0d64108f19fb5c0e2b5739cf4b8bf12e8f1d
khalid almuanen <kly48170@GMAIL.COM>
2025-11-29 07:38:59 +0300

    feat: Randevu sistemi ve üye randevu ekranları eklendi

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -1,11 +1,17 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace FitnessCenterApp.Models
 {
     public enum AppointmentStatus
     {
+        [Display(Name = "Beklemede")]
         Pending = 0,
+
+        [Display(Name = "Onaylandı")]
         Approved = 1,
+
+        [Display(Name = "Reddedildi")]
         Rejected = 2
     }
 
@@ -13,34 +19,47 @@ namespace FitnessCenterApp.Models
     {
         public int Id { get; set; }
 
-        // FK
         [Required]
+        [Display(Name = "Üye")]
         public string MemberId { get; set; }
 
         [Required]
+        [Display(Name = "Antrenör")]
         public int TrainerId { get; set; }
 
         [Required]
+        [Display(Name = "Hizmet")]
         public int ServiceId { get; set; }
 
-        // Appointment Time
         [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Tarih")]
         public DateTime Date { get; set; }
 
         [Required]
+        [DataType(DataType.Time)]
+        [Display(Name = "Başlangıç Saati")]
         public TimeSpan StartTime { get; set; }
 
         [Required]
+        [DataType(DataType.Time)]
+        [Display(Name = "Bitiş Saati")]
         public TimeSpan EndTime { get; set; }
 
-        // Approval Mechanism
+        [Display(Name = "Durum")]
         public AppointmentStatus Status { get; set; } = AppointmentStatus.Pending;
 
+        [MaxLength(500)]
+        [Display(Name = "Notlar")]
         public string? Notes { get; set; }
 
-        // Navigation
-        public ApplicationUser Member { get; set; }
-        public Trainer Trainer { get; set; }
-        public Service Service { get; set; }
+        [Display(Name = "Üye")]
+        public ApplicationUser? Member { get; set; }
+
+        [Display(Name = "Antrenör")]
+        public Trainer? Trainer { get; set; }
+
+        [Display(Name = "Hizmet")]
+        public Service? Service { get; set; }
     }
 }
